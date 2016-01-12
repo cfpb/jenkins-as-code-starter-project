a312209b3152a98d82aabc2588cd90824a5dcb62
Irina Muchnik <irina.muchnik@cfpb.gov>
2016-01-12 13:56:17 -0500

    Adding placeholder script, so that src/main/groovy will be included in git

---
+++ src/main/groovy/placeholder.groovy
@@ -0,0 +1,7 @@
+/**
+ * Created by muchniki on 1/12/16.
+ */
+class placeholder {
+    //THIS just a placeholder for now, but this is a good place to put all your helper code
+    //other than the job scripts itself, because this will automatically be "on the path"
+}
