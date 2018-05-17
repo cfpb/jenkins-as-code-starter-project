c846922fa30cb5b072ed1c883255bc2334bc9de3
Marc Esher <marc.esher@cfpb.gov>
2018-05-17 09:16:11 -0400

    Change examples to reflect preference for pipelines; name consistently; remove unnecessary duplication in freestyle jobs

--- jobs/jobs.groovy
+++ jobs/jobs.groovy
@@ -18,28 +18,35 @@ if (env.isDev()) {
 // Our custom builders are simply wrappers around job-dsl job types (https://github.com/jenkinsci/job-dsl-plugin)
 // In the `with {}` closure, you have full access to the job-dsl API (https://jenkinsci.github.io/job-dsl-plugin/)
 // and anything else job-dsl supports, such as the `configure` block (https://github.com/jenkinsci/job-dsl-plugin/wiki/The-Configure-Block)
-new FlowJobBuilder(
-        name: 'GeneratedFlowJob',
-        description: 'this our first stab at it',
-        jobs: ['job1', 'job2']
+
+def pipelineScript = """
+    pipeline {
+        agent { label 'master' }
+        stages {
+            stage('hello') {
+                steps {
+                    sh 'echo "Hello World"'
+                }
+            }
+        }
+    }
+"""
+
+new PipelineJobBuilder(
+        name: 'Hello-Pipeline-Job',
+        description: 'Example Pipeline job',
+        pipelineScript: pipelineScript,
+        sandboxFlag: false
 ).build(this).with {
     // example of extending a job with the job-dsl api
     logRotator {
-        numToKeep(365)
+        numToKeep(60)
     }
 }
 
 new BaseJobBuilder(
-        name: "job1",
-        description: "One job"
-).build(this).with {
-    steps {
-        shell("echo ${env.getEnv()}")
-    }
-}
-new BaseJobBuilder(
-        name: "job2",
-        description: "Two job"
+        name: "Hello-Freestyle-Job",
+        description: "Example Freestyle job"
 ).build(this).with {
     steps {
         shell("echo ${env.getEnv()}")
